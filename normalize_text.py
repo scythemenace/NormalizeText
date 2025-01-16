356627d51fc40781855862c329a2c37c95d6c576
Ankur Pandey <pankur216@gmail.com>
2025-01-15 22:24:50 -0500

    Completed part 2.1 & 2.2

---
+++ normalize_text.py
@@ -0,0 +1,64 @@
+# Taking file input from command line
+import sys
+import nltk
+import string
+
+file = open(sys.argv[1])
+content = file.read()
+file.close()
+
+stemmer = None
+lemmatizer = None
+lower = True
+stopwords = None
+punctuation = None
+
+for arg in sys.argv:
+    match arg:
+        case "-s":
+            stemmer = nltk.stem.PorterStemmer()
+        case "-lr":
+            lemmatizer = nltk.stem.WordNetLemmatizer()
+        case "-l":
+            lower = True
+        case "-st":
+            stopwords = nltk.corpus.stopwords.words("english")
+        case "p":
+            punctuation = True
+
+tokens = nltk.word_tokenize(content)
+
+if lower:
+    tokens = [token.lower() for token in tokens]
+
+if stemmer:
+    tokens = [stemmer.stem(token) for token in tokens]
+
+if lemmatizer:
+
+    def get_wordnet_pos(tag):
+        if tag.startswith("J"):
+            return nltk.corpus.wordnet.ADJ
+        elif tag.startswith("V"):
+            return nltk.corpus.wordnet.VERB
+        elif tag.startswith("N"):
+            return nltk.corpus.wordnet.NOUN
+        elif tag.startswith("R"):
+            return nltk.corpus.wordnet.ADV
+        else:
+            return None
+
+    pos_tags = nltk.pos_tag(tokens)
+
+    tokens = [
+        lemmatizer.lemmatize(token, pos=pos) if pos else token
+        for token, tag in pos_tags
+        for pos in [get_wordnet_pos(tag)]
+    ]
+
+
+if stopwords:
+    tokens = [token for token in tokens if token.lower() not in stopwords]
+
+if punctuation:
+    tokens = [token for token in tokens if token not in string.punctuation]
